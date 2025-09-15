128f5dfaaa6d9e06d7ea051cf82c5495acd056bc
Dipesh Aryal <aryaldeelep@gmail.com>
2025-09-15 13:54:13 +0545

    chore: change next cofig to ignore eslint and ts during builds

--- realtime-next/next.config.ts
+++ realtime-next/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import { tr } from "zod/v4/locales";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typedRoutes: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
